236452ee52a210ca28e9183a74ead66e75f6d328
seth-beaty <seth.a.beaty@gmail.com>
2024-12-24 13:09:25 -0500

    B: Added count chars

--- main.py
+++ main.py
@@ -6,15 +6,28 @@ def get_book_text(path):
 def get_num_words(text):
 
     words = text.split()
-    return len(words)       
+    return len(words) 
+
+
+def count_chars(text):
+    character_dict = {}
+    lowered_text = text.lower()
+    for char in lowered_text:
+        if char in character_dict:
+            character_dict[char] += 1
+        else:
+            character_dict[char] = 1
+
+    return character_dict
+
 
 def main():
 
     book_path = "./books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
     num_words = get_num_words(text)
-    print(f"{num_words} in the document")
+    characters = count_chars(text)
+    print(characters)
 
     
 
