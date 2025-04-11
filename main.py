09d441234247a974a17a67ee046ea08f24a8cf64
seth-beaty <seth.a.beaty@gmail.com>
2025-04-11 17:23:08 -0400

    Moved most functions to stats.py and implemented command line args

--- main.py
+++ main.py
@@ -1,59 +1,15 @@
-from stats import get_num_words
-
-
-def get_book_text(path):
-
-    with open(path) as f:
-        return f.read()
-
-
-def count_chars(text):
-    character_dict = {}
-    lowered_text = text.lower()
-    for char in lowered_text:
-        if char in character_dict:
-            character_dict[char] += 1
-        else:
-            character_dict[char] = 1
-
-    return character_dict
-
-
-def sort_on(dict):
-    return dict["num"]
-
-
-def get_book_report(book_path, char_dict):
-
-    char_list = []
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document\n")
-
-    for char in char_dict:
-        if char.isalpha():
-            char_list.append({"name": char,
-                              "num": char_dict[char]})
-            
-    char_list.sort(reverse=True, key=sort_on)
-
-    for char in char_list:
-        print(f"The {char['name']} character was found {char['num']} times")
-    
-    print("--- End report ---")
-
+from stats import count_chars, get_book_report, get_book_text, get_num_words
+import sys
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    print(f"{num_words} words found in the document\n")
+    # book_path = "books/frankenstein.txt"
+    text = get_book_text(sys.argv[1])
     characters = count_chars(text)
-    print(f"{characters}")
-    # get_book_report(book_path, characters)
+    get_book_report(sys.argv[1], characters)
 
     
 
--- stats.py
+++ stats.py
@@ -14,3 +14,49 @@ def get_num_words(text):
     total_words = [word.strip(",.!?;:'\"-") for word in total_words]  # Remove leading/trailing punctuation
    
     return len(total_words)
+
+def count_chars(text):
+    character_dict = {}
+    lowered_text = text.lower()
+    for char in lowered_text:
+        if char in character_dict:
+            character_dict[char] += 1
+        else:
+            character_dict[char] = 1
+
+    return character_dict
+
+
+def get_book_report(book_path, char_dict):
+
+    char_list = []
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print(f"----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print(f"--------- Character Count -------")
+
+    for char in char_dict:
+        if char.isalpha():
+            char_list.append({"name": char,
+                              "num": char_dict[char]})
+            
+    char_list.sort(reverse=True, key=sort_on)
+
+    for char in char_list:
+        print(f"{char['name']}: {char['num']}")
+    
+    print("============= END ===============")
+
+
+def get_book_text(path):
+
+    with open(path) as f:
+        return f.read()
+
+
+def sort_on(dict):
+    return dict["num"]
